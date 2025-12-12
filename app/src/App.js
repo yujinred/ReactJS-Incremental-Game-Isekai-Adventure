bd8c3d67c0e5b737e05c88d3a63e4827eaaaf5b4
Eugene Zhang <eugene.zhang8@gmail.com>
2025-12-12 03:03:15 -0500

    [Unrelated] Learning ReactJS tutorial
    Going through the TicTacToe Tutorial

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const cat = {
+  name: 'wawa',
+  imageUrl: 'https://i.imgur.com/wzeCD5O.jpeg',
+  imageSize: 90
+};
+
 
 function App() {
   return (
@@ -21,5 +29,77 @@ function App() {
     </div>
   );
 }
+function MyButton({ setIsLoggedIn }) {
 
-export default App;
+  function handleClick() {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <button onClick={handleClick}>Click for a Surprise</button>
+  );
+}
+
+
+function Wawa() {
+  return (
+    <>
+    <h1>{cat.name}</h1>
+    <img 
+      className="catImage"
+      src={cat.imageUrl}
+      alt={'This is where I would put ' + cat.name + ' IF THE IMAGE LOADS!'}
+      style={{
+        width: cat.imageSize,
+        height: cat.imageSize
+      }}
+      />
+    </>
+  );
+}
+
+const monsters = [
+  { title: 'Pikachu', isPokemon: true, id: 1},
+  { title: 'Lucario', isPokemon: true,  id: 2},
+  { title: 'Angelmon', isPokemon: false,  id: 3},
+];
+const listItems = monsters.map(mons => 
+  <li key={mons.id} style={{ color: mons.isPokemon? 'red' : 'blue'}}>
+    {mons.title}
+  </li>
+);
+
+export default function MyApp() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='welcomeText'>Welcome to my app</h1>
+        {isLoggedIn? (
+          <Wawa />
+        ) : (
+          <MyButton setIsLoggedIn={setIsLoggedIn} />
+        )}
+      </header>
+    </div>
+  );
+}
+
+
+
+// export default function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
